417318dd80d6ad9a0504bedc3780541371491acd
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-03-18 14:50:02 +0100

    Add input classes

---
+++ ClassicTetris/ClassicTetris/Inputs/Actions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Input;
+
+namespace ClassicTetris
+{
+    public class Actions
+    {
+        private Actions Instance = null;
+
+        public readonly Dictionary<Action, Bind> Binds = new Dictionary<Action, Bind>();
+
+        public Actions GetInstance()
+        {
+            if (Instance == null)
+                Instance = new Actions();
+            return Instance;
+        }
+
+        private Actions()
+        {
+            Binds[Action.Left] = new Bind(Keys.Left, Keys.A);
+            Binds[Action.Right] = new Bind(Keys.Right, Keys.D);
+            Binds[Action.Down] = new Bind(Keys.Down, Keys.S);
+            Binds[Action.Rotate] = new Bind(Keys.Up, Keys.W);
+        }
+
+        public void Update(KeyboardState state)
+        {
+            foreach(Bind bind in Binds.Values)
+            {
+                bind.Update(state);
+            }
+        }
+    }
+
+    public enum Action
+    {
+        Left,
+        Right,
+        Down,
+        Rotate,
+    }
+}
---
+++ ClassicTetris/ClassicTetris/Inputs/Bind.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace ClassicTetris
+{
+    public class Bind
+    {
+        private readonly Keys[] Keys;
+        private bool Current;
+        private bool Previous;
+
+        public Bind(params Keys[] keys)
+        {
+            this.Keys = keys;
+            this.Current = false;
+            this.Previous = false;
+        }
+
+        public void Update(KeyboardState state)
+        {
+            Previous = Current;
+            Current = false;
+            foreach (Keys k in Keys)
+            {
+                if (state.IsKeyDown(k))
+                {
+                    Current = true;
+                    break;
+                }
+            }
+        }
+
+        public bool IsPressed()
+        {
+            return Current == true && Previous == false;
+        }
+
+        public bool IsReleased()
+        {
+            return Current == true && Previous == false;
+        }
+
+        public bool IsDown()
+        {
+            return Current;
+        }
+
+        public bool IsUp()
+        {
+            return !Current;
+        }
+
+    }
+}
