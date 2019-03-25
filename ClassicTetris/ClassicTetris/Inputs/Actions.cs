1557520383d8632878a1d801a57d1af251b66dc4
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-03-25 12:59:12 +0100

    add comments for the inputs and split files

--- ClassicTetris/ClassicTetris/Inputs/Actions.cs
+++ ClassicTetris/ClassicTetris/Inputs/Actions.cs
@@ -6,25 +6,37 @@ namespace ClassicTetris
 {
     public class Actions
     {
-        private Actions Instance = null;
 
-        public readonly Dictionary<Action, Bind> Binds = new Dictionary<Action, Bind>();
+        private static readonly Dictionary<Action, Bind> Binds = new Dictionary<Action, Bind>();
 
-        public Actions GetInstance()
+        /// <summary>
+        /// Create every actions with the possibles inputs
+        /// </summary>
+        static Actions()
         {
-            if (Instance == null)
-                Instance = new Actions();
-            return Instance;
-        }
-
-        private Actions()
-        {
-            Binds[Action.Left] = new Bind(Keys.Left, Keys.A);
+			// Possible input with the keyboard
+			Binds[Action.Left] = new Bind(Keys.Left, Keys.A);
             Binds[Action.Right] = new Bind(Keys.Right, Keys.D);
             Binds[Action.Down] = new Bind(Keys.Down, Keys.S);
             Binds[Action.Rotate] = new Bind(Keys.Up, Keys.W);
         }
+        
+        /// <summary>
+        /// This indexer return the bind for a specific action
+        /// </summary>
+        /// <param name="action">Action.</param>
+		public Bind this[Action action]
+		{
+			get
+			{
+				return Binds[action];
+			}
+		}
 
+        /// <summary>
+        /// Update every bind with the specified keyboard state
+        /// </summary>
+        /// <param name="state">State.</param>
         public void Update(KeyboardState state)
         {
             foreach(Bind bind in Binds.Values)
@@ -32,13 +44,5 @@ namespace ClassicTetris
                 bind.Update(state);
             }
         }
-    }
-
-    public enum Action
-    {
-        Left,
-        Right,
-        Down,
-        Rotate,
     }
 }
--- ClassicTetris/ClassicTetris/Inputs/Bind.cs
+++ ClassicTetris/ClassicTetris/Inputs/Bind.cs
@@ -7,12 +7,19 @@ using Microsoft.Xna.Framework.Input;
 
 namespace ClassicTetris
 {
+	/// <summary>
+	/// Contain a state for a specific action <see cref="T:ClassicTetris.Actions"/> class.
+    /// </summary>
     public class Bind
     {
         private readonly Keys[] Keys;
         private bool Current;
         private bool Previous;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ClassicTetris.Bind"/> class.
+        /// </summary>
+        /// <param name="keys">Keys.</param>
         public Bind(params Keys[] keys)
         {
             this.Keys = keys;
@@ -20,6 +27,10 @@ namespace ClassicTetris
             this.Previous = false;
         }
 
+        /// <summary>
+        /// Update the bind for this keyboard state
+        /// </summary>
+        /// <param name="state"></param>
         public void Update(KeyboardState state)
         {
             Previous = Current;
@@ -34,21 +45,37 @@ namespace ClassicTetris
             }
         }
 
+        /// <summary>
+        /// Is the bind in a pressed state, triggered on per input
+        /// </summary>
+        /// <returns><c>true</c>, if pressed was ised, <c>false</c> otherwise.</returns>
         public bool IsPressed()
         {
             return Current == true && Previous == false;
         }
 
+        /// <summary>
+		/// Is the bind released, triggered on per input
+        /// </summary>
+        /// <returns><c>true</c>, if released was ised, <c>false</c> otherwise.</returns>
         public bool IsReleased()
         {
             return Current == true && Previous == false;
         }
 
+        /// <summary>
+        /// Is the bind down, true every frame while the key is down
+        /// </summary>
+        /// <returns><c>true</c>, if down was ised, <c>false</c> otherwise.</returns>
         public bool IsDown()
         {
             return Current;
         }
 
+        /// <summary>
+        /// Is the bind up, true every frame while the key is up
+        /// </summary>
+        /// <returns><c>true</c>, if up was ised, <c>false</c> otherwise.</returns>
         public bool IsUp()
         {
             return !Current;
