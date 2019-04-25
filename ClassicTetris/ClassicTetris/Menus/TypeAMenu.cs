b691162e5d1a63906a8e1c7889b6841110bc76a1
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-04-25 15:21:48 +0200

    add moving cursor for the select level menu

--- ClassicTetris/ClassicTetris/Menus/TypeAMenu.cs
+++ ClassicTetris/ClassicTetris/Menus/TypeAMenu.cs
@@ -15,15 +15,34 @@ namespace ClassicTetris.Menus
         private SpriteBatch sb;
         private Texture2D bg;
 
+		private Texture2D rect;
+
+		private int selectedIndex;
+		private const int maxmenu = 10;
+		private const int sizeX = 55;
+        private const int sizeY = sizeX;
+
+		private const int offsetX = 213;
+		private const int offsetY = 305;
+		private const int border = 9;
+
         public TypeAMenu(Tetris tetris)
         {
             this.tetris = tetris;
+			selectedIndex = 0;
+            
+            
+            
         }
 
         public void Draw(GameTime gameTime)
         {
             sb.Begin();
             sb.Draw(bg, Vector2.Zero, Color.White);
+            
+			int x = offsetX + (sizeX + border) * (selectedIndex % (maxmenu / 2));
+			int y = offsetY + (sizeX + border) * (selectedIndex / (maxmenu / 2));
+			sb.Draw(rect, new Vector2(x,y), Color.White);
             
             sb.End();
         }
@@ -36,6 +55,13 @@ namespace ClassicTetris.Menus
         {
             sb = new SpriteBatch(graphicDevice);
             bg = Content.Load<Texture2D>("Textures/typeAMenu");
+
+
+			rect = new Texture2D(sb.GraphicsDevice, sizeX, sizeY);
+			Color[] data = new Color[sizeX * sizeY];
+            Color color = new Color(255, 127, 0, 127);
+            for (int k = 0; k < data.Length; ++k) data[k] = color;
+            rect.SetData(data);
         }
 
         public void Start()
@@ -54,10 +80,39 @@ namespace ClassicTetris.Menus
         {
             if (Actions.GetInstance()[Inputs.Action.MenuValidate].IsPressed())
             {
+				//set level
                 tetris.ChangeMenu(EMenu.Game);
             }
 
-            // Todo selection
+			if (Actions.GetInstance()[Inputs.Action.MenuValidate].IsPressed())
+            {
+                tetris.ChangeMenu(EMenu.TypeAMenu);
+            }
+            if (Actions.GetInstance()[Inputs.Action.MenuDown].IsPressed())
+            {
+				if (selectedIndex < maxmenu / 2)
+                {
+                    selectedIndex += maxmenu / 2;
+                }
+            }
+            if (Actions.GetInstance()[Inputs.Action.MenuUp].IsPressed())
+            {
+				if(selectedIndex >= maxmenu / 2)
+				{
+					selectedIndex -= maxmenu / 2;
+				}
+            }
+            if (Actions.GetInstance()[Inputs.Action.MenuRight].IsPressed())
+            {
+				selectedIndex += 1;
+				selectedIndex %= maxmenu;
+            }
+            if (Actions.GetInstance()[Inputs.Action.MenuLeft].IsPressed())
+            {
+				selectedIndex -= 1;
+				selectedIndex = TetrisMath.mod(selectedIndex, maxmenu);
+            }
+
         }
     }
 }
